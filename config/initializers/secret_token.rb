b4e593028562ed1235ae5c0731445aae55f7eb31
hack <hackforsake@gmail.com>
2013-03-08 21:48:05 +0530

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'c93bcb1ccf2deb6c21a8f516ee761c619c2a6d8a400d2770d574e0142405633f60a7a10c26b0fe1067fd74656304eb4b9f8dd85e805b9582a4013087c2e29c51'
